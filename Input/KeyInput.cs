1514b5a525e6a34af0c2e735387068ed0aca9dd3
Samuel Oechsler <samuel@boxnetwork.de>
2017-01-19 17:21:43 +0100

    Added key input

---
+++ Input/KeyInput.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace BareKit.Input
+{
+	public class KeyInput : Input
+	{
+		KeyboardState previousState;
+		KeyboardState currentState;
+		Keys key;
+
+		public KeyInput(InputState inputState, Keys key) : base(inputState)
+		{
+			previousState = currentState = Keyboard.GetState();
+			this.key = key;
+		}
+
+		public override void Update()
+		{
+			base.Update();
+
+			switch (TriggerState)
+			{
+				case InputState.Pressed:
+					if (currentState.IsKeyDown(key) && previousState.IsKeyUp(key))
+						Trigger();
+					break;
+				case InputState.Released:
+					if (currentState.IsKeyUp(key) && previousState.IsKeyDown(key))
+						Trigger();
+					break;
+				case InputState.Down:
+					if (currentState.IsKeyDown(key))
+						Trigger();
+					break;
+			}
+
+			previousState = currentState;
+			currentState = Keyboard.GetState();
+		}
+	}
+}
