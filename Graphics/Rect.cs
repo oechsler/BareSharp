f9a828e90f6755438294573de81f6c50dda88056
Samuel Oechsler <samuel@boxnetwork.de>
2017-02-05 01:02:56 +0100

    Added documentation to rect

--- Graphics/Rect.cs
+++ Graphics/Rect.cs
@@ -23,19 +23,25 @@ namespace BareKit.Graphics
 		{
 			base.Draw(buffer);
 
+			// Determine wether the Rect should be rendered 
 			RotatedRectangle screenBounds = Scaling.Bounds;
 			screenBounds.ChangePosition((int)(-Scaling.Size.X / 2), (int)(-Scaling.Size.Y / 2));
 			if (screenBounds.Intersects(Bounds) && Alpha > 0)
 			{
 				isRendered = true;
 
+				// Allocate a offscreen SpriteBatch buffer for the Rect
+				// if none existed to this point
 				if (primitiveBuffer == null)
 					primitiveBuffer = new SpriteBatch(buffer.GraphicsDevice);
 
+				// Calculate the transform of the Rect
 				Matrix transfrom = Matrix.CreateTranslation(-Size.X / 2 + Origin.X, -Size.Y / 2 + Origin.Y, 1) *
 								   Matrix.CreateRotationZ(Rotation) *
 								   Matrix.CreateTranslation(Scaling.Size.X / 2 + Position.X, Scaling.Size.Y / 2 + Position.Y, 0);
 
+				// Apply the transform to the SpriteBatch buffer and
+				// render the primitive rectangle shape to it
 				primitiveBuffer.Begin(transformMatrix: transfrom);
 
 				if (filled)
@@ -49,28 +55,48 @@ namespace BareKit.Graphics
 				isRendered = false;
 		}
 
+		/// <summary>
+		/// Gets or sets a value indicating whether the Rect is filled.
+		/// </summary>
+		/// <value>The fill indicating value.</value>
 		public bool Filled
 		{
 			get { return filled; }
 			set { filled = value; }
 		}
 
+		/// <summary>
+		/// Gets or sets the tickness displayed onscreen.
+		/// </summary>
+		/// <value>The tickness displayed onscreen.</value>
 		public float Tickness
 		{
 			get { return thickness; }
 			set { thickness = value; }
 		}
-		
+
+		/// <summary>
+		/// Gets the size vector.
+		/// </summary>
+		/// <returns>The size vector.</return
 		public Vector2 Size
 		{
 			get { return Scale * Scaling.Scale; }
 		}
 
+		/// <summary>
+		/// Gets the bounds rectangle.
+		/// </summary>
+		/// <value>The bounds rectangle.</value>
 		public RotatedRectangle Bounds
 		{
 			get { return new RotatedRectangle(new Rectangle((int)(Position.X - Size.X / 2), (int)(Position.Y - Size.Y / 2), (int)Size.X, (int)Size.Y), Rotation); }
 		}
 
+		/// <summary>
+		/// Gets the value indicating whether the Rect is rendered.
+		/// </summary>
+		/// <value>The render indicating value.</value>
 		public bool IsRendered
 		{
 			get { return isRendered; }
