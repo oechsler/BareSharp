1713e4ba33c958b968a51fa06e9d70f7a1687800
Samuel Oechsler <samuel@boxnetwork.de>
2017-02-05 01:02:32 +0100

    Added documentation to container

--- Graphics/Container.cs
+++ Graphics/Container.cs
@@ -19,20 +19,29 @@ namespace BareKit.Graphics
         {
             base.Draw(buffer);
 
+			// Allocate a offscreen SpriteBatch buffer for the Container
+			// if none existed to this point
             if (containerBuffer == null)
                 containerBuffer = new SpriteBatch(buffer.GraphicsDevice);
 
+			// Calculate the transform of the Container
 			Matrix transfrom = Matrix.CreateTranslation(-Scaling.Size.X / 2 + Origin.X, -Scaling.Size.Y / 2 + Origin.Y, 1) *
 						       Matrix.CreateRotationZ(Rotation) *
 						       Matrix.CreateTranslation(Scaling.Size.X / 2 + Position.X, Scaling.Size.Y / 2 + Position.Y, 0) *
 						       Matrix.CreateScale(Scale.X, Scale.Y, 1);
 
+			// Apply the transform to the SpriteBatch buffer and
+			// render the contained Drawables to it
 			containerBuffer.Begin(transformMatrix: transfrom);
             foreach (Drawable drawable in drawables)
                 drawable.Draw(containerBuffer);
             containerBuffer.End();
         }
 
+		/// <summary>
+		/// Adds a child Drawable to the Container.
+		/// </summary>
+		/// <param name="child">The child Drawable being added.</param>
         public Container AddChild(Drawable child)
         {
 			child.Initialize(Scaling);
@@ -42,6 +51,10 @@ namespace BareKit.Graphics
             return this;
         }
 
+		/// <summary>
+		/// Removes a child Drawable from the Container.
+		/// </summary>
+		/// <param name="child">The child Drawable being removed.</param>
         public Container RemoveChild(Drawable child)
         {
             child.Parent = null;
@@ -50,6 +63,10 @@ namespace BareKit.Graphics
             return this;
         }
 
+		/// <summary>
+		/// Returs a copy of the contianed child Drawables.
+		/// </summary>
+		/// <value>The list of child Drawables.</value>
         public List<Drawable> Children
         {
             get { return new List<Drawable>(drawables); }
