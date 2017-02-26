59dd8b1d390dad6bffa6662f5b5aaadae1f9318b
Samuel Oechsler <samuel@boxnetwork.de>
2017-02-26 20:16:37 +0100

    Added documentation to inputmanager

--- Input/InputManager.cs
+++ Input/InputManager.cs
@@ -10,6 +10,10 @@ namespace BareKit.Input
 
 		ScalingManager scaling;
 
+        /// <summary>
+        /// Initializes a new instance of the InputManager class.
+        /// </summary>
+        /// <param name="scaling">The ScalingManager attached to the Inputs.</param>
 		public InputManager(ScalingManager scaling)
 		{
 			inputs = new List<Input>();
@@ -17,12 +21,19 @@ namespace BareKit.Input
 			this.scaling = scaling;
 		}
 
+        /// <summary>
+        /// Sends an updatecall to the attached Inputs.
+        /// </summary>
 		public void Update()
 		{
 			foreach (Input input in inputs)
 				input.Update();
 		}
 
+        /// <summary>
+		/// Adds a child Input to the InputManager.
+		/// </summary>
+		/// <param name="child">The child Input being added.</param>
 		public InputManager AddChild(Input child)
 		{
 			child.Initialize(scaling);
@@ -31,6 +42,10 @@ namespace BareKit.Input
 			return this;
 		}
 
+        /// <summary>
+		/// Removes a child Input to the InputManager.
+		/// </summary>
+		/// <param name="child">The child Input being removed.</param>
 		public InputManager RemoveChild(Input child)
 		{
 			inputs.Remove(child);
