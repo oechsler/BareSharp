4a97ac31bae2872a3ad1bd2b072eae8e4ce8a33c
Samuel Oechsler <samuel@boxnetwork.de>
2017-02-26 20:23:40 +0100

    Added documentation to soundmanager

--- Audio/SoundManager.cs
+++ Audio/SoundManager.cs
@@ -6,17 +6,27 @@ namespace BareKit.Audio
     {
         List<Sound> sounds;
 
+        /// <summary>
+        /// Initializes a new instance of the SoundManager class.
+        /// </summary>
         public SoundManager()
         {
 			sounds = new List<Sound>();
         }
 
+        /// <summary>
+        /// Sends an updatecall to the Sounds.
+        /// </summary>
         public void Update()
         {
 			foreach (Sound sound in sounds)
 				sound.Update();
         }
 
+        /// <summary>
+        /// Adds a child Sound to the SoundManager.
+        /// </summary>
+        /// <param name="child">The child Sound being added.</param>
 		public SoundManager AddChild(Sound sound)
         {
 			sounds.Add(sound);
@@ -24,6 +34,10 @@ namespace BareKit.Audio
 			return this;
         }
 
+        /// <summary>
+        /// Removes a child Sound to the SoundManager.
+        /// </summary>
+        /// <param name="child">The child Sound being removed.</param>
 		public SoundManager RemoveChild(Sound sound)
 		{
 			sounds.Remove(sound);
@@ -31,6 +45,9 @@ namespace BareKit.Audio
 			return this;
 		}
 
+        /// <summary>
+        /// Stops all playing sounds.
+        /// </summary>
 		public SoundManager Stop()
 		{
 			foreach (Sound sound in sounds)
