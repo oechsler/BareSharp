32a61c38cad4ce95d852ac847a0e12fd68793ab6
Samuel Oechsler <samuel@boxnetwork.de>
2017-03-12 18:44:22 +0100

    Added general logger

---
+++ Utils/Logger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+#if WINDOWS_UAP
+using System.Diagnostics;
+#endif
+
+namespace BareKit
+{
+    public static class Logger
+    {
+        public static void Info(Type sender, string message)
+        {
+            message = $"[{DateTime.Now.ToString("HH:mm:ss")}][{sender.Name}]> {message}";
+#if WINDOWS_UAP
+            Debug.WriteLine(message);
+#else
+            Console.WriteLine(message);
+#endif
+        }
+
+        public static void Warn(Type sender, string message)
+        {
+            message = $"[{DateTime.Now.ToString("HH:mm:ss")}][{sender.Name.ToUpper()}]> {message}";
+#if WINDOWS_UAP
+            Debug.WriteLine(message);
+#else
+            Console.WriteLine(message);
+#endif
+        }
+    }
+}
