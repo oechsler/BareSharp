b5b7b6f1fbe25d05acc472a7e0ebfb1143e3c7f8
Samuel Oechsler <samuel@boxnetwork.de>
2017-05-01 23:21:10 +0200

    Cleaned code in boot script

--- Utils/Scripts/boot.lua
+++ Utils/Scripts/boot.lua
@@ -1,62 +1,92 @@
-﻿-- mscorlib.dll
-alloc('System.Type', 'mscorlib')
-bare.timeSpan = alloc('System.TimeSpan', 'mscorlib')
-alloc('System.IO.Stream', 'mscorlib')
-bare.streamReader = alloc('System.IO.StreamReader', 'mscorlib')
-bare.streamWriter = alloc('System.IO.StreamWriter', 'mscorlib')
-if bare.timeSpan == nil then
-	-- .Net Core (System.Runtime)
-	alloc('System.Type', 'System.Runtime')
-	bare.timeSpan = alloc('System.TimeSpan', 'System.Runtime')
-	alloc('System.IO.Stream', 'System.Runtime')
-	bare.streamReader = alloc('System.IO.StreamReader', 'System.Runtime')
-	bare.streamWriter = alloc('System.IO.StreamWriter', 'System.Runtime')
+﻿local _MSCORLIB = "mscorlib"
+local _RUNTIME = "System.Runtime"
+local _MONOGAME = "MonoGame.Framework"
+local _IMPORT = {}
+
+function _IMPORT.core()
+	-- Allocate core resources on ".Net" platforms
+	alloc("System.IO.Stream", _MSCORLIB)
+	alloc("System.Type", _MSCORLIB)
+
+	bare.streamReader = alloc("System.IO.StreamReader", _MSCORLIB)
+	bare.streamWriter = alloc("System.IO.StreamWriter", _MSCORLIB)
+	bare.timeSpan = alloc("System.TimeSpan", _MSCORLIB)
+
+	-- Allocate core resources on ".Net Core" platforms
+	if not bare.timeSpan then
+		alloc("System.IO.Stream", _RUNTIME)
+		alloc("System.Type", _RUNTIME)
+
+		bare.streamReader = alloc("System.IO.StreamReader", _RUNTIME)
+		bare.streamWriter = alloc("System.IO.StreamWriter", _RUNTIME)
+		bare.timeSpan = alloc("System.TimeSpan", _RUNTIME)
+	end
+
+	-- Allocate core modules from MonoGame.Framework
+	alloc("Microsoft.Xna.Framework.Content.ContentManager", _MONOGAME)
+	alloc("Microsoft.Xna.Framework.Game", _MONOGAME)
+	alloc("Microsoft.Xna.Framework.GameWindow", _MONOGAME)
+	alloc("Microsoft.Xna.Framework.Graphics.Effect", _MONOGAME)
+	alloc("Microsoft.Xna.Framework.GraphicsDeviceManager", _MONOGAME)
+
+	bare.blendMode = alloc("Microsoft.Xna.Framework.Graphics.BlendState", _MONOGAME)
+	bare.color = alloc("Microsoft.Xna.Framework.Color", _MONOGAME)
+	bare.rectangle = alloc("Microsoft.Xna.Framework.Rectangle", _MONOGAME)
+	bare.rotatedRectangle = alloc("Microsoft.Xna.Framework.RotatedRectangle", _DEFAULT)
+	bare.vector2 = alloc("Microsoft.Xna.Framework.Vector2", _MONOGAME)
+	bare.vector3 = alloc("Microsoft.Xna.Framework.Vector3", _MONOGAME)
 end
 
--- MonoGame.Framework.dll
-alloc('Microsoft.Xna.Framework.Game', 'MonoGame.Framework')
-alloc('Microsoft.Xna.Framework.GraphicsDeviceManager', 'MonoGame.Framework')
-alloc('Microsoft.Xna.Framework.GameWindow', 'MonoGame.Framework')
-bare.vector2 = alloc('Microsoft.Xna.Framework.Vector2', 'MonoGame.Framework')
-bare.vector3 = alloc('Microsoft.Xna.Framework.Vector3', 'MonoGame.Framework')
-bare.color = alloc('Microsoft.Xna.Framework.Color', 'MonoGame.Framework')
-bare.blendMode = alloc('Microsoft.Xna.Framework.Graphics.BlendState', 'MonoGame.Framework')
-alloc('Microsoft.Xna.Framework.Graphics.Effect', 'MonoGame.Framework')
-alloc('Microsoft.Xna.Framework.Content.ContentManager', 'MonoGame.Framework')
-bare.rotatedRectangle = alloc('Microsoft.Xna.Framework.RotatedRectangle', _DEFAULT)
+function _IMPORT.base()
+	-- Allocate the base modules from BareKit
+	bare.database = alloc("BareKit.Database", _DEFAULT)
+	bare.storage = alloc("BareKit.Storage", _DEFAULT)
+end
 
--- BareKit
-bare.storage = alloc('BareKit.Storage', _DEFAULT)
-bare.database = alloc('BareKit.Database', _DEFAULT)
+function _IMPORT.audio()
+	-- Allocate the audio modules from BareKit.Audio
+	alloc("BareKit.Audio.SoundManager", _DEFAULT)	
 
--- BareKit.Audio
-bare.sound = alloc('BareKit.Audio.Sound', _DEFAULT)
-alloc('BareKit.Audio.SoundManager', _DEFAULT)
+	bare.sound = alloc("BareKit.Audio.Sound", _DEFAULT)
+end
 
--- BareKit.Graphics
-bare.container = alloc('BareKit.Graphics.Container', _DEFAULT)
-alloc('BareKit.Graphics.Drawable', _DEFAULT)
-bare.label = alloc('BareKit.Graphics.Label', _DEFAULT)
-bare.rect = alloc('BareKit.Graphics.Rect', _DEFAULT)
-bare.scalingManager = alloc('BareKit.Graphics.ScalingManager', _DEFAULT)
-bare.shader = alloc('BareKit.Graphics.Shader', _DEFAULT)
-bare.scene = alloc('BareKit.Graphics.Scene', _DEFAULT)
-bare.sprite = alloc('BareKit.Graphics.Sprite', _DEFAULT)
+function _IMPORT.graphics()
+	--Allocate the graphics modules from BareKit.Graphics
+	alloc("BareKit.Graphics.Drawable", _DEFAULT)
 
--- BareKit.Input
-bare.gamepadInput = alloc('BareKit.Input.GamepadInput', _DEFAULT)
-bare.buttons = alloc('Microsoft.Xna.Framework.Input.Buttons', 'MonoGame.Framework')
-alloc('BareKit.Input.Input', _DEFAULT)
-bare.inputState = alloc('BareKit.Input.InputState', _DEFAULT)
-alloc('BareKit.Input.InputManager', _DEFAULT)
-bare.keyInput = alloc('BareKit.Input.KeyInput', _DEFAULT)
-bare.keys = alloc('Microsoft.Xna.Framework.Input.Keys', 'MonoGame.Framework')
-bare.touchInput = alloc('BareKit.Input.TouchInput', _DEFAULT)
-bare.finger = alloc('BareKit.Input.Finger', _DEFAULT)
+	bare.container = alloc("BareKit.Graphics.Container", _DEFAULT)
+	bare.label = alloc("BareKit.Graphics.Label", _DEFAULT)
+	bare.rect = alloc("BareKit.Graphics.Rect", _DEFAULT)
+	bare.scalingManager = alloc("BareKit.Graphics.ScalingManager", _DEFAULT)
+	bare.scene = alloc("BareKit.Graphics.Scene", _DEFAULT)
+	bare.shader = alloc("BareKit.Graphics.Shader", _DEFAULT)
+	bare.sprite = alloc("BareKit.Graphics.Sprite", _DEFAULT)
+end
 
--- Embedded libraries
-class = require(_DEFAULT .. ".Utils.Scripts.30log", true)
-tween = require(_DEFAULT .. ".Utils.Scripts.tween", true)
+function _IMPORT.input()
+	-- Allocate the input modules from BareKit.Input
+	alloc("BareKit.Input.Input", _DEFAULT)
+	alloc("BareKit.Input.InputManager", _DEFAULT)
 
--- Load main.lua
+	bare.buttons = alloc("Microsoft.Xna.Framework.Input.Buttons", _MONOGAME)
+	bare.finger = alloc("BareKit.Input.Finger", _DEFAULT)
+	bare.gamepadInput = alloc("BareKit.Input.GamepadInput", _DEFAULT)
+	bare.inputState = alloc("BareKit.Input.InputState", _DEFAULT)
+	bare.keyInput = alloc("BareKit.Input.KeyInput", _DEFAULT)
+	bare.keys = alloc("Microsoft.Xna.Framework.Input.Keys", _MONOGAME)
+	bare.touchInput = alloc("BareKit.Input.TouchInput", _DEFAULT)
+end
+
+function _IMPORT.embedded()
+	-- Allocate the embedded libraries from BareKit.Utils.Scripts
+	class = require(_DEFAULT .. ".Utils.Scripts.30log", true)
+	tween = require(_DEFAULT .. ".Utils.Scripts.tween", true)
+end
+
+-- Load each module from the import list
+for _, module in pairs(_IMPORT) do
+	module()
+end
+
+-- Require the main module
 require("main")
